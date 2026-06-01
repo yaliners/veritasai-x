6ac5beabeb38cbf3c9241e7b589ab79012a29c70
yalinersy <yalinesuje@gmail.com>
2026-06-01 13:24:33 +0530

    Add CSS import to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { getRouter } from "./router";
+import "./styles.css";
 
 const router = getRouter();
 
